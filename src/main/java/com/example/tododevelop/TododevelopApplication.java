f9fd89c4b1d940e016c681c8c60f97beddbb6b9e
Doritos <mydjkim@naver.com>
2025-02-13 01:11:41 +0900

    Init: 프로젝트 시작

---
+++ src/main/java/com/example/tododevelop/TododevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tododevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TododevelopApplication.class, args);
+    }
+
+}
